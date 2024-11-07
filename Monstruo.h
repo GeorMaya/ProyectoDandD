ca50c2920bae20320afd519982a2f75b8c3c38bb
GeorMaya <119626984+GeorMaya@users.noreply.github.com>
2024-11-06 21:11:47 -0600

    Update Monstruo.h
    se cambió el tipo de dato de los numericos a double

--- Monstruo.h
+++ Monstruo.h
@@ -3,33 +3,26 @@
 template <typename T>
 class Monstruo {
 public:
-    Monstruo() : left(nullptr), right(nullptr), cr(T()), ac(T()), hp(T()) {}
+    Monstruo() : left(nullptr), right(nullptr), cr(0), ac(0), hp(0) {}
 
-    Monstruo(T name, T cr, T type, T size, T ac, T hp, T align)
+    Monstruo(T name, double cr, T type, T size, double ac, double hp, T align)
         : name(name), cr(cr), type(type), size(size), ac(ac), hp(hp), align(align), left(nullptr), right(nullptr) {}
 
-    ~Monstruo() {
-    }
+    ~Monstruo() {}
 
-    T getCr() {
-        return cr;
-    }
-
-    T getNombre() {
-        return name;
-    }
-
-    T getType() {
-        return type;
-    }
-
-    T getHp() {
-        return hp;
-    }
+    // Métodos getter para los atributos
+    T getNombre() { return name; }
+    double getCr() { return cr; }
+    T getType() { return type; }
+    T getSize() { return size; }
+    double getAc() { return ac; }
+    double getHp() { return hp; }
+    T getAlign() { return align; }
 
     Monstruo* left;
     Monstruo* right;
 
 private:
-    T name, type, size, align, cr, ac, hp;
+    T name, type, size, align;  // Atributos de tipo genérico
+    double cr, ac, hp;          // Atributos específicos de tipo numérico
 };
