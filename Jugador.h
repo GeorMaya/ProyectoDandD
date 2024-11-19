4e8d63a0680d148ebf2e3cee0f3b989370d23a2d
GeorMaya <119626984+GeorMaya@users.noreply.github.com>
2024-11-19 14:13:46 -0600

    Update Jugador.h
    19/11/24

--- Jugador.h
+++ Jugador.h
@@ -1,39 +1,46 @@
+
 #pragma once
 #include "Calabozo.h"
 
-template <typename T>
+template<typename T>
 class Jugador {
-public:
-    Jugador(T raza = T(), T name = T(), T hp = T(), T mp = T())
-        : raza(raza), name(name), hp(hp), mp(mp), head(nullptr), tail(nullptr) {}
-
-    ~Jugador() {
-        Cuarto<T>* current = head;
-        while (current != nullptr) {
-            Cuarto<T>* next = current->right;
-            if (current->getMonstruo()) delete current->getMonstruo();
-            delete current;
-            current = next;
-        }
-    }
-
-    bool agregarCuarto(Cuarto<T>* cuarto) {
-        if (!cuarto) return false;
-
-        if (head == nullptr) {
-            head = cuarto;
-            tail = cuarto;
-        } else {
-            tail->right = cuarto;
-            cuarto->left = tail;
-            tail = cuarto;
-        }
-        return true;
-    }
-
 private:
-    T raza, name, hp, mp;
+    T raza;
+    T nombre;
+    double hp;
+    double mp;
 
     Cuarto<T>* head;
     Cuarto<T>* tail;
+
+public:
+    Jugador(T raza=T(), T nombre=T(), double hp = double(), double mp = double())
+        : raza(raza), nombre(nombre), hp(hp), mp(mp), head(nullptr), tail(nullptr) {}
+
+    ~Jugador() {
+        Cuarto<T>* temp = head;
+        while (temp != nullptr) {
+            Cuarto<T>* next = temp->right;
+            if (temp -> getMonstruos())
+                delete temp->getMonstruos();
+            delete temp;
+            temp = next;
+        }
+    }
+
+    bool addCuarto(Cuarto<T>* cuarto) {
+        if(!cuarto)
+            return false;
+
+        if(head == nullptr) {
+            head == cuarto;
+            tail == cuarto;
+        }
+        else {
+            tail->right = cuarto;
+            cuarto->left = tail;
+            tail=cuarto;
+        }
+        return true;
+    }
 };
